c71ad2baa26045c261a466a6e89e17c837a2700c
AnActualBanana <loganmcclain59@gmail.com>
2023-02-13 10:42:03 -0600

    more

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,47 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "sort.h"
 
-#define BASE 10
-#define get_digit(num, place) (num / (int)pow(BASE, place) % BASE)
+#define BUCKETS 10
+#define GET_DIGIT(x, y) (((x) / (int)pow(10, y)) % 10)
 
-void radix_sort(int *array, size_t size) {
-  int max = array[0];
-  for (size_t i = 1; i < size; i++) {
-    if (array[i] > max) {
-      max = array[i];
+/**
+ * radix_sort - sorts an array of integers in ascending order using radix sort
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+    int i, j, k, digit, m = 0, n = 0;
+    listint_t *buckets[BUCKETS];
+    listint_t *tmp, *head, *tail;
+
+    if (!array || size < 2)
+        return;
+
+    for (i = 0; i < (int)size; i++)
+    {
+        tmp = malloc(sizeof(*tmp));
+        if (!tmp)
+            return;
+        tmp->n = array[i];
+        tmp->prev = tmp->next = NULL;
+        buckets[0] = tmp;
+        n++;
+        for (j = 0; j < n; j++)
+        {
+            head = tail = buckets[j];
+            while (tail->next)
+                tail = tail->next;
+            for (k = 0; k < BUCKETS; k++)
+                buckets[k] = NULL;
+            n = 0;
+            while (head)
+            {
+                digit = GET_DIGIT(head->n, m);
+                if (!buckets[digit])
+                {
+                    buckets[digit] = head;
+                    n++;
+                }
+                else
+                {
+                    tail = buckets[digit];
+                    while (tail->next)
+                        tail = tail->next;
+                    tail->next = head;
+                }
+                tmp = head->next;
+                head->next = NULL;
+                head = tmp;
+            }
+        }
+        m++;
+        n = 0;
+        for (k = 0; k < BUCKETS; k++)
+        {
+            head = buckets[k];
+            while (head)
+            {
+                array[n++] = head->n;
+                tmp = head;
+                head = head->next;
+                free(tmp);
+            }
+        }
+        print_array(array, size);
     }
-  }
-
-  int num_digits = (int)log10(max) + 1;
-  int *temp = malloc(size * sizeof(int));
-
-  for (int place = 0; place < num_digits; place++) {
-    int count[BASE] = {0};
-
-    for (size_t i = 0; i < size; i++) {
-      count[get_digit(array[i], place)]++;
-    }
-
-    for (int i = 1; i < BASE; i++) {
-      count[i] += count[i - 1];
-    }
-
-    for (int i = size - 1; i >= 0; i--) {
-      int digit = get_digit(array[i], place);
-      temp[--count[digit]] = array[i];
-    }
-
-    for (size_t i = 0; i < size; i++) {
-      array[i] = temp[i];
-    }
-
-    printf("Sorted array after significant digit %d:\n", place + 1);
-    for (size_t i = 0; i < size; i++) {
-      printf("%d ", array[i]);
-    }
-    printf("\n");
-  }
-
-  free(temp);
 }
